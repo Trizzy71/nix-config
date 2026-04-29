2470dea1c1515822f2cd3c632fa7e2a221cc2602
tristan <tristan.m.peterson@gmail.com>
2026-04-29 03:28:16 -0500

    cleaned up # in dell-config

--- dell-configuration.nix
+++ dell-configuration.nix
@@ -1,32 +1,25 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
 { config, lib, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ # Import hardware-configuration.nix
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
+  # Systemd + EFI boot #
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos-test"; # Define your hostname.
 
-  # Configure network connections interactively with nmcli or nmtui.
+  #### Networking (nmcli | nmtui) ####
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  #### Time Zone ####
   time.timeZone = "America/Chicago";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
+  #### Internationalisation ####
   # i18n.defaultLocale = "en_US.UTF-8";
   # console = {
   #   font = "Lat2-Terminus16";
@@ -34,7 +27,7 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
-  # Enable the X11 windowing system.
+  #### X11 + WM ####
   # services.xserver.enable = true;
   services.xserver = {
     enable = true;
@@ -43,14 +36,14 @@
 
   
 
-  # Configure keymap in X11
+  #### Keymap ####
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
-  # Enable CUPS to print documents.
+  #### Printing ####
   # services.printing.enable = true;
 
-  # Enable sound.
+  #### Sound ####
   # services.pulseaudio.enable = true;
   # OR
   # services.pipewire = {
@@ -58,7 +51,7 @@
   #   pulse.enable = true;
   # };
 
-  # Enable touchpad support (enabled default in most desktopManager).
+  #### Touchpad support (enabled default in most desktopManager) ####
   # services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -71,71 +64,19 @@
     ];
   };
 
-  ## ## \/ Programs you can Edit \/ ## ##
-
+  #### Programs ####
   programs.firefox.enable = true;
   programs.zsh.enable = true;
-  # users.users.tristan = {
-    # isNormalUser = true;
-    # extraGroups  = [ "wheel" "networkManager" ];
-    # shell        = pkgs.zsh; # sets zsh as login shell
-  # };
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
+  #### System Profile Packages ####
   environment.systemPackages = with pkgs; [
-    # nano
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    # lf
-    # btop
-    alacritty
-    # bat
-    # cmatrix
+    vim        #
+    # wget     # DONT TOUCH
+    alacritty  #
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.11"; # Did you read the comment?
+  #### System state and Experimental features ####
+  system.stateVersion = "25.11";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 }
