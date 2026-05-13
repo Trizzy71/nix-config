88d2c42b99cdd4228df498c99a965bbf0c52639a
Trizzy71 <tristan.m.peterson@gmail.com>
2026-05-13 05:36:56 -0500

    theres the wofi haha

---
+++ modules/wofi.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+  programs.wofi = {
+    enable = true;
+    settings = {
+      width = 500;
+      height = 300;
+      location = "center";
+      show = "drun";
+      allow_markup = true;
+      allow_images = true;
+    };
+  };
+}
