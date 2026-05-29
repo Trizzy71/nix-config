7c093553d00a951dc035282233d8d697316e57df
tristan <tristan.m.peterson@gmail.com>
2026-05-28 22:03:46 -0500

    Have to start small

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,34 +1,9 @@
 { config, pkgs, ... }:
 
-# Tailscale is going to need some future configuration
-
-let
-  tailscaleAPIKey = config.tailscale.apiKey or null;
-in
+# -TAILSCALE-
 {
-  options.tailscale.apiKey = mkOption {
-    type = types.str;
-    description = "API key for Tailscale";
-    default = null; # we can define a default here in the future
-  };
-
-  environment.systemPackages = with pkgs; [
+  home.packages = with pkgs; [
     tailscale
   ];
 
-  # Additional cconfig
-  services.tailscale.enable = true;
-  services.tailscale.apiKey = tailscaleAPIKey;
-
-  # Tailscale up command?
-  systemd.services.tailscale = {
-    description = "Tailscale Service";
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.tailscale}/bin/tailscale up --authkey=${tailscaleAPIKey}";
-      Restart = "always";
-    };
-    wantedBy = [ "multi-user.target" ];
-  };
 }
