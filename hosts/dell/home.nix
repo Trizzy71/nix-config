9c823282b6ec3b600a7663d1579108de049ec8a2
tristan <tristan.m.peterson@gmail.com>
2026-05-28 23:24:35 -0500

    Update home.nix

--- hosts/dell/home.nix
+++ hosts/dell/home.nix
@@ -7,11 +7,12 @@
     ../../modules/shell.nix
     ../../modules/git.nix
     ../../modules/neovim.nix
+    ../../modules/tailscale.nix
   ];
 
-  home.username      = "tristan";
+  home.username = "tristan";
   home.homeDirectory = "/home/tristan";
-  home.stateVersion  = "25.11";
+  home.stateVersion = "25.11";
 
   programs.home-manager.enable = true;
 }
