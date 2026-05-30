fee0dd3bd7bfb01454f114ecdafb21861d51ae52
tristan <tristan.m.peterson@gmail.com>
2026-05-29 23:13:03 -0500

    future

---
+++ modules/exit-node.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+# -EXIT NODE-
+# shared config for all exit nodes
+
+{
+  imports = [
+    ./ssh-system.nix
+    ./tailscale-system.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.networkmanager.enable = true;
+  time.timeZone = "America/Chicago";
+
+  users.users.tristan = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    shell = pkgs.zsh;
+  };
+
+  programs.zsh.enable = true;
+
+  environment.systemPackages = with pkgs; [ vim ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "25.11";
+}
