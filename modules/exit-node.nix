885fd365b1a5e7632fb0f7c1012a9099b077b077
tristan <tristan.m.peterson@gmail.com>
2026-05-31 18:37:43 -0500

    skip tpm job

--- modules/exit-node.nix
+++ modules/exit-node.nix
@@ -15,6 +15,8 @@
   networking.networkmanager.enable = true;
   time.timeZone = "America/Chicago";
 
+  systemd.tpm2.enable = false;
+
   users.users.tristan = {
     isNormalUser = true;
     extraGroups = [
